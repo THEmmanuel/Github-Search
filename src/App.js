af27c0c9e4d099797db59a73a2ff33b4344e8d52
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-06-09 15:35:39 +0100

    added searchButton component and style module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import SearchButton from './Components/SearchButton/searchButton';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
-    </div>
+      <SearchButton/>
+    </div> 
   );
 }
 
---
+++ src/Components/SearchButton/searchButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './searchButton.module.css';
+
+const SearchButton = props => 
+    <button className = {style.SearchButton}>
+        Search
+    </button>
+
+
+export default SearchButton;

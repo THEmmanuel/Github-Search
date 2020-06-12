34eca9596f03b00f7207b5a31b515c6ccaf4ad49
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-06-12 20:05:40 +0100

    added axios, react-router and react-router-dom dependencies

---
+++ src/Components/UserInfo/UserInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProfilePicture from '../ProfilePicture/ProfilePicture';
+import style from './UserInfo.module.css';
+
+const userInfo = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default userInfo

d06c55eff9fd46e6a0482d0e849e37bd0cc81019
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-06-22 03:36:01 +0100

    Refactored RepositoryResults to UserInfo

---
+++ src/Containers/UserInfo/UserInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//What to do here:
+/*
+I need to pass the username as props
+I need to fetch the repository information based on the username passed
+Display the user's repositories  
+*/
+
+
+const RepositoryResults = (props) => {
+    return (
+        <div></div>
+    )
+}
+
+export default RepositoryResults;

ea964b5ff45f3607cac5ad7627ba8cbc45ed953e
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-14 14:50:03 +0100

    Removed unused import from RepositoryCard

--- src/Containers/UserDetails/UserDetails.js
+++ src/Containers/UserDetails/UserDetails.js
@@ -2,7 +2,6 @@ import React from 'react';
 import style from './UserDetails.module.css';
 import UserAccountInfo from '../../Components/UserAccountInfo/UserAccountInfo';
 import UserInfo from '../UserInfo/UserInfo';
-import RepositoryCard from '../../Components/Cards/RepositoryCard/RepositoryCard';
 
 const UserDetails = (props) => {
     return (

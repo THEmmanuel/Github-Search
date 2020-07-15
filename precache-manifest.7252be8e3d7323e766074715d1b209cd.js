1f5b048cf36b1088bf2081970e493f8eeebfa5be
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-15 03:06:00 +0100

    Updates

---
+++ precache-manifest.7252be8e3d7323e766074715d1b209cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be847e6bde6ddf57b4013598bffa418b",
+    "url": "/Github-Search/index.html"
+  },
+  {
+    "revision": "a4a00253d33425fc5a06",
+    "url": "/Github-Search/static/css/main.11c81ed6.chunk.css"
+  },
+  {
+    "revision": "030bc7fa5eddc8599d0d",
+    "url": "/Github-Search/static/js/2.db72203e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Github-Search/static/js/2.db72203e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a00253d33425fc5a06",
+    "url": "/Github-Search/static/js/main.50f121ed.chunk.js"
+  },
+  {
+    "revision": "96da89f4335e56900008",
+    "url": "/Github-Search/static/js/runtime-main.d44d76c0.js"
+  }
+]);
